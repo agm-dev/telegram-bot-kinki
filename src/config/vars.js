ac603056b33b7cf0f9dacc6782155087373eb890
Adrian Gonzalo <gonzahh@gmail.com>
2020-01-02 16:59:15 +0100

    fixed dotenv config

--- src/config/vars.js
+++ src/config/vars.js
@@ -2,7 +2,7 @@ const { join } = require('path');
 
 require('dotenv-safe').config({
   example: join(__dirname, '..', '..', '.env.example'),
-  require: join(__dirname, '..', '..', '.env'),
+  path: join(__dirname, '..', '..', '.env'),
 });
 
 const config = {
